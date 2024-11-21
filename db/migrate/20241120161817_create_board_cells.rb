00140e88322043012d5090b4197a6b5129bb50ee
Ryan Vo <ryanvo.0162@gmail.com>
2024-11-21 10:10:30 +0700

    Replace SQLite with MySQL in Gemfile and database configuration, update board_cells migration

--- db/migrate/20241120161817_create_board_cells.rb
+++ db/migrate/20241120161817_create_board_cells.rb
@@ -1,7 +1,7 @@
 class CreateBoardCells < ActiveRecord::Migration[8.0]
   def change
     create_table :board_cells do |t|
-      t.references :board, null: false, foreign_key: true
+      t.integer :board_id
       t.integer :row
       t.integer :column
       t.boolean :mine
